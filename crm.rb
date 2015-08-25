243984950fcaf4dd74c0b84ae9e59d509f6ccb6b
Christina <christina@Christinas-Mac-Air.local>
2015-08-25 17:17:31 -0400

    second commit, I think

--- crm.rb
+++ crm.rb
@@ -1,14 +1,19 @@
 require 'sinatra'
 
-get '/' do 
-	"Main Menu"
-end
+# get '/' do 
+# 	"Main Menu"
+# end
 
-get '/' do 
-	"Hello world"
-end
+# get '/' do 
+# 	"Hello world"
+# end
+
+# get '/' do
+# 	erb :index
+# end
 
 get '/' do
+	@crm_app_name = "My CRM"
 	erb :index
 end
 
