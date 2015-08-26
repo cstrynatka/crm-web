e5162f4f92951f772970589995f7965d8115b6dd
Christina <christina@ChristiasMacAir.lan>
2015-08-25 20:05:51 -0400

    Possibly on my fourth commit?

--- contact.rb
+++ contact.rb
@@ -5,14 +5,14 @@ class Contact
 attr_accessor :first_name, :last_name, :email, :notes
 attr_reader :id
 
-def self.run(name)
-	crm = new(name)
-	crm.main_menu
-end
+# def self.run(name)
+# 	crm = new(name)
+# 	crm.main_menu
+# end
 
-def initialize(name)
-	@title = name 
-end
+# def initialize(name)
+# 	@title = name 
+# end
 
 def initialize(first_name, last_name, email, notes)
 @first_name = first_name
@@ -20,4 +20,8 @@ def initialize(first_name, last_name, email, notes)
 @email = email
 @notes = notes
 	end
+
+def full_name
+	first_name + " " + last_name
+end
 end
--- crm.rb
+++ crm.rb
@@ -1,24 +1,25 @@
 require_relative 'contact'
 require 'sinatra'
 
-get '/' do 
-	"contacts"
+get "/contacts" do 
+	@contacts = []
+	@contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+	@contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+	@contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
+	erb :contacts
 end
 
 # get '/' do 
 # 	"Main Menu"
 # end
 
-# get '/' do 
-# 	"Hello world"
-# end
-
 # get '/' do
 # 	erb :index
 # end
 
-# get '/' do
-# 	@crm_app_name = "My CRM"
-# 	erb :index
-# end
+get '/' do
+	@crm_app_name = "My CRM"
+	erb :index
+end
 
